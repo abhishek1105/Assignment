862b6b835eb91a99499f9a7549ef21f28a232494
abhishek <abhishek.pathak1105@gmail.com>
2018-08-20 14:05:14 +0530

    Assignment

---
+++ Demoo.java
@@ -0,0 +1,19 @@
+public class Demoo
+ {
+	public static void main(String[] x)
+	 {
+		System.out.println("hello world");
+	 }
+ }
+ 
+ import java.util.Scanner;
+public class Variable
+ {
+	public static void main(String[] x)
+	 {
+		 Scanner y=new Scanner(System.in);
+		 int a;
+		 
+		 
+		 System.out.println("enter value for a");
+		 a=y.nextInt();
